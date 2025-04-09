47700d281229c6a2497c4524d955ca6fea9b4ed6
Oscarfabrod <153616063+Oscarfabrod@users.noreply.github.com>
2025-04-09 19:34:16 +0200

    Delete demo-of/demo-of directory

--- demo-of/demo-of/src/main/java/com/example/ofabregat/demo_of/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ofabregat.demo_of.repository;
-
-import com.example.ofabregat.demo_of.modelProducto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Producto, Integer>{
-
-
-}
